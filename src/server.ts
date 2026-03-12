f1181ca87cd19aeee2fe34b5afd23a749062764e
NyanLinHtet <nyanlindemon@gmail.com>
2026-03-12 20:09:01 +0630

    database add

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects.js";
+import cors from "cors";
 
 const app = express();
 const Port = 8000;
 
+if (!process.env.FRONTEND_URL) {
+  throw new Error("Frontend URL is not defined");
+}
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use("/api/subjects", subjectsRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
